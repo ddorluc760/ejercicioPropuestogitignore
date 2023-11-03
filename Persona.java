45dd457cf94590e98ae611325e11d024a1e01ad4
ddorluc760 <ddorluc760@iesalmudeyne.es>
2023-11-03 10:57:41 +0100

    Primer commit en el cual subo todos los proyectos excepto el pdf

---
+++ Persona.java
@@ -0,0 +1,14 @@
+package personas;
+
+public class Persona {
+
+	public static void main(String[] args) {
+		int edad = 20;
+		if(edad < 18) {
+			System.out.println("La persona es menor de edad");
+		}else {
+			System.out.println("La persona es mayor de edad");
+		}
+	}
+
+}
